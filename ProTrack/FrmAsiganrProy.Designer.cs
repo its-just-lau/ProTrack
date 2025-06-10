1609897f2e83db307c1dc3823485bd3cac0226c2
Dono <donovanixo@gmail.com>
2025-06-10 15:33:22 -0600

    Cambios para que funcione FrmAsiganrProy

--- ProTrack/FrmAsiganrProy.Designer.cs
+++ ProTrack/FrmAsiganrProy.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cbxEstudiantes = new System.Windows.Forms.ComboBox();
+            this.cbxProyectos = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -41,80 +41,91 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 16;
             this.label1.Text = "Estudiante";
             // 
-            // comboBox1
+            // cbxEstudiantes
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(30, 36);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(309, 28);
-            this.comboBox1.TabIndex = 17;
+            this.cbxEstudiantes.FormattingEnabled = true;
+            this.cbxEstudiantes.Location = new System.Drawing.Point(20, 23);
+            this.cbxEstudiantes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxEstudiantes.Name = "cbxEstudiantes";
+            this.cbxEstudiantes.Size = new System.Drawing.Size(207, 21);
+            this.cbxEstudiantes.TabIndex = 17;
             // 
-            // comboBox2
+            // cbxProyectos
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(30, 102);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(309, 28);
-            this.comboBox2.TabIndex = 19;
+            this.cbxProyectos.FormattingEnabled = true;
+            this.cbxProyectos.Location = new System.Drawing.Point(20, 66);
+            this.cbxProyectos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxProyectos.Name = "cbxProyectos";
+            this.cbxProyectos.Size = new System.Drawing.Size(207, 21);
+            this.cbxProyectos.TabIndex = 19;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 79);
+            this.label2.Location = new System.Drawing.Point(9, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 18;
             this.label2.Text = "Proyecto";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(450, 166);
+            this.button1.Location = new System.Drawing.Point(300, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 35);
+            this.button1.Size = new System.Drawing.Size(69, 23);
             this.button1.TabIndex = 20;
             this.button1.Text = "Cancelar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(328, 166);
+            this.button2.Location = new System.Drawing.Point(219, 108);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 35);
+            this.button2.Size = new System.Drawing.Size(69, 23);
             this.button2.TabIndex = 21;
             this.button2.Text = "Asignar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = global::ProTrack.Properties.Resources.mdi__book_add;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(462, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(308, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(91, 91);
+            this.pictureBox1.Size = new System.Drawing.Size(61, 59);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
             // 
             // FrmAsiganrProy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(235)))), ((int)(((byte)(245)))));
-            this.ClientSize = new System.Drawing.Size(565, 213);
+            this.ClientSize = new System.Drawing.Size(377, 138);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cbxProyectos);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbxEstudiantes);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmAsiganrProy";
             this.Text = "Asignar";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmAsiganrProy_FormClosed);
+            this.Load += new System.EventHandler(this.FrmAsiganrProy_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -125,8 +136,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cbxEstudiantes;
+        private System.Windows.Forms.ComboBox cbxProyectos;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- ProTrack/FrmAsiganrProy.cs
+++ ProTrack/FrmAsiganrProy.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ProTrack;
 
 namespace ProTrack
 {
@@ -15,6 +18,130 @@ namespace ProTrack
         public FrmAsiganrProy()
         {
             InitializeComponent();
+        }
+
+        private async void FrmAsiganrProy_Load(object sender, EventArgs e)
+        {
+            ClienteWS.AlRecibirRespuestaEstado += ManejarRespuesta;
+
+            await ClienteWS.EnviarAsync(new { accion = "listar_estudiantes" });
+            await ClienteWS.EnviarAsync(new { accion = "listar_proyectos_asesor" });
+
+        }
+
+        private void ManejarRespuesta(string estado, object datos)
+        {
+            if (this.IsDisposed) return;
+
+            this.Invoke((MethodInvoker)(() =>
+            {
+                if (estado == "exito")
+                {
+                    List<Dictionary<string, string>> lista = null;
+
+                    try
+                    {
+                        if (datos is JArray jArray)
+                        {
+                            lista = jArray.ToObject<List<Dictionary<string, string>>>();
+                        }
+                        else if (datos is string jsonString)
+                        {
+                            // Intentar deserializar solo si parece JSON (empieza con [ o {)
+                            jsonString = jsonString.Trim();
+                            if (jsonString.StartsWith("[") || jsonString.StartsWith("{"))
+                            {
+                                lista = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(jsonString);
+                            }
+                            else
+                            {
+                                // No es JSON, mostrar mensaje o ignorar
+                                MessageBox.Show(jsonString, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            var json = JsonConvert.SerializeObject(datos);
+                            lista = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(json);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al procesar datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    if (lista == null || lista.Count == 0)
+                    {
+                        MessageBox.Show("No se recibieron datos.");
+                        return;
+                    }
+
+                    if (lista[0].ContainsKey("id_estudiante"))
+                    {
+                        var estudiantes = lista
+                            .Select(e => new
+                            {
+                                id_estudiante = e["id_estudiante"],
+                                nombre = e["nombre"]
+                            })
+                            .ToList();
+
+                        cbxEstudiantes.DataSource = estudiantes;
+                        cbxEstudiantes.DisplayMember = "nombre";
+                        cbxEstudiantes.ValueMember = "id_estudiante";
+                    }
+                    else if (lista[0].ContainsKey("id_proyecto"))
+                    {
+                        var proyectos = lista
+                            .Select(p => new
+                            {
+                                id_proyecto = p["id_proyecto"],
+                                nombre = p["nombre"]
+                            })
+                            .ToList();
+
+                        cbxProyectos.DataSource = proyectos;
+                        cbxProyectos.DisplayMember = "nombre";
+                        cbxProyectos.ValueMember = "id_proyecto";
+                    }
+                }
+                else if (estado == "error")
+                {
+                    MessageBox.Show(datos.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }));
+        }
+
+
+        private void FrmAsiganrProy_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ClienteWS.AlRecibirRespuestaEstado -= ManejarRespuesta;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (cbxEstudiantes.SelectedItem is null || cbxProyectos.SelectedItem is null)
+            {
+                MessageBox.Show("Selecciona un estudiante y un proyecto.");
+                return;
+            }
+
+            int idEst = Convert.ToInt32(cbxEstudiantes.SelectedValue);
+            int idProy = Convert.ToInt32(cbxProyectos.SelectedValue);
+
+            var solicitud = new
+            {
+                accion = "asignar_proyecto_estudiante",
+                datos = new
+                {
+                    id_estudiante = idEst,
+                    id_proyecto = idProy
+                }
+            };
+
+            await ClienteWS.EnviarAsync(solicitud);
         }
     }
 }
